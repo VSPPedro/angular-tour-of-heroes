28edeb94adee9291c6d0adc9ff6e669facd45233
Pedro Paiva <pedrovsp109@gmail.com>
2019-09-07 15:11:25 -0300

    Add "5. Routing" tutorial page

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
-import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
@@ -15,5 +19,10 @@ export class HeroService {
     return of(HEROES);
   }
 
-  constructor(private messageService: MessageService) { }
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+
 }
